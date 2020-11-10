6a0e39f51caf69dccc91ac307041add275c43122
Alexander124234 <60904096+Alexander124234@users.noreply.github.com>
2020-11-10 11:47:27 -0600

    Add files via upload

---
+++ Site_Selector v0.3.bat
@@ -0,0 +1,73 @@
+@echo off
+color 1f
+title Site Selector by Alexander Miota
+:top
+echo ***************************************************************
+echo.
+echo Site Selector v0.3
+echo.
+echo ***************************************************************
+echo.
+echo Key: [1] Google - Search Engine
+echo [2] Hotmail - Mail Server
+echo [3] Yahoo - Search Engine/Mail Server
+echo [4] Facebook - Social Networking
+echo [5] Myspace - Social Networking
+echo [6] CNN - News
+echo [7] Weather - Weather
+echo [8] WikiHow - A How-To Website
+echo [9] Instructables - A How-To Website
+echo [10] YouTube - Online Videos
+echo [11] Answers - Online Encyclopedia
+echo [12] Wikipedia - Online Encyclopedia
+echo [13] Gmail - Mail Server
+echo [14] Twitter - social networking
+echo [15] foxnews - News
+echo.
+echo [e] Exit
+echo.
+echo ***************************************************************
+echo Enter the number of the website which you would like to go to:
+echo.
+set /p udefine= 
+echo.
+echo ***************************************************************
+if %udefine%==1 start www.google.com
+if %udefine%==2 start www.hotmail.com
+if %udefine%==3 start www.yahoo.com
+if %udefine%==4 start www.facebook.com
+if %udefine%==5 start www.myspace.com
+if %udefine%==6 start www.cnn.com
+if %udefine%==7 start www.weather.com
+if %udefine%==7 start www.wikihow.com
+if %udefine%==9 start www.instructables.com
+if %udefine%==10 start www.youtube.com
+if %udefine%==11 start www.answers.com
+if %udefine%==12 start www.wikipedia.com
+if %udefine%==13 start https://mail.google.com
+if %udefine%==14 start https://twitter.com
+if %udefine%==15 start https://www.foxnews.com
+if %udefine%==e goto exit
+
+cls
+echo ***************************************************************
+echo.
+echo Thank You for using Site Selector by Alexander Miota
+echo.
+echo ***************************************************************
+echo Type [e] to exit or [b] to go back and select another site.
+echo.
+set /p udefine=
+echo.
+echo ***************************************************************
+if %udefine%==b goto top
+if %udefine%==e goto exit
+:exit
+cls
+echo ***************************************************************
+echo.
+echo Thank You for using Site Selector by Alexander Miota
+echo.
+echo ***************************************************************
+pause
+exit
